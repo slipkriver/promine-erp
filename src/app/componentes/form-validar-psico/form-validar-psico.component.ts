30910710b1735cbfb19d5d5106716117589f24f3
byros21 <byros3@yahoo.com>
2022-07-05 11:04:47 -0500

    formulario de verificacion de psicologia, sin estructura ni data

---
+++ src/app/componentes/form-validar-psico/form-validar-psico.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-validar-psico',
+  templateUrl: './form-validar-psico.component.html',
+  styleUrls: ['./form-validar-psico.component.scss'],
+})
+export class FormValidarPsicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
